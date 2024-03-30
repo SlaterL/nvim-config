9c814a1c7d68fb8b2b3a705354711a2dcc507dc6
SlaterL <slater@slater3.0>
2024-03-30 16:04:47 -0600

    add hop

---
+++ lua/plugins/hop.lua
@@ -0,0 +1,9 @@
+return {
+	"smoka7/hop.nvim",
+	version = "*",
+	opts = {},
+	keys = {
+		{ "fw", "<cmd>HopWord<cr>", desc = "[H]op [W]ord" },
+		{ "fl", "<cmd>HopWordCurrentLine<cr>", desc = "[H]op [C]urrent line" },
+	},
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -80,18 +80,8 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 
 		-- Git
-		vim.keymap.set(
-			"n",
-			"<leader>Gc",
-			"<cmd>Telescope git_commits<CR>",
-			{ desc = '[S]earch Recent Files ("." for repeat)' }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>Gs",
-			"<cmd>Telescope git_status<CR>",
-			{ desc = '[S]earch Recent Files ("." for repeat)' }
-		)
+		vim.keymap.set("n", "<leader>Gc", "<cmd>Telescope git_commits<CR>", { desc = "[G]it [C]ommits" })
+		vim.keymap.set("n", "<leader>Gs", "<cmd>Telescope git_status<CR>", { desc = "[G]it [S]tatus" })
 		-- Slightly advanced example of overriding default behavior and theme
 		vim.keymap.set("n", "/", function()
 			-- You can pass additional configuration to telescope to change theme, layout, etc.
