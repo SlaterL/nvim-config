aba8c63b459837a8d9dd5926f9415083cc76fca6
SlaterL <slater@slater3.0>
2024-03-31 19:27:00 -0600

    keys for hop

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -3,7 +3,8 @@ return {
 	version = "*",
 	opts = {},
 	keys = {
-		{ "<leader>hw", "<cmd>HopWord<cr>", desc = "[H]op [W]ord", noremap = true },
-		{ "<leader>hl", "<cmd>HopWordCurrentLine<cr>", desc = "[H]op Current [L]ine", noremap = true },
+		{ "f", "<cmd>HopWord<cr>", desc = "[H]op [W]ord", noremap = true },
+		{ "F", "<cmd>HopChar1<cr>", desc = "[H]op [L]etter", noremap = true },
+		{ "<C-f>", "<cmd>HopAnywhere<cr>", desc = "[H]op [A]nywhere", noremap = true },
 	},
 }
